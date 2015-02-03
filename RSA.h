3a74c64ad55c4d4a4b6fa97c14be0e0cb90f4957
ideawu <wuzuyang@gmail.com>
2015-02-03 12:56:11 +0800

    Create RSA.h

---
+++ RSA.h
@@ -0,0 +1,16 @@
+//
+//  RSA.h
+//  My
+//
+//  Created by ideawu on 15-2-3.
+//  Copyright (c) 2015年 ideawu. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface RSA : NSObject
+
++ (NSString *)encryptString:(NSString *)str publicKey:(NSString *)pubKey;
++ (NSString *)encryptData:(NSData *)data publicKey:(NSString *)pubKey;
+
+@end
