0bf812894a682704ea9057f7bd24aad516b69a26
ideawu <wuzuyang@gmail.com>
2015-02-03 12:57:08 +0800

    Create Demo.m

---
+++ Demo.m
@@ -0,0 +1,5 @@
+	#import "RSA.h"
+	
+	NSString *pubkey = @"-----BEGIN PUBLIC KEY-----\nMIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQDLuwt30JLYFvKcFOUdjPuDRdqv\nSnDb5TSdA/w0ND/GwLExpT66DeRz9+6//G//Y0y3c/yWT14k/ab1vID4U6W3vOgr\nafC0RyuIgH8ooCTNQpU+LtIoZ6qCejnux7VZ5lwWeT/9DQjWOtf6TopeRdzmOX09\nwa7c5xGGUsmi29QxDQIDAQAB\n-----END PUBLIC KEY-----";
+	NSString *ret = [RSA encryptString:@"hello world!" publicKey:pubkey];
+	NSLog(@"encrypted: %@", ret);
