bb85db789c91a410e236242c148d975fa15a9ca8
ideawu <ideawu@me>
2016-06-14 11:44:33 +0800

    update

--- README.md
+++ README.md
@@ -3,14 +3,6 @@ Doing RSA encryption with Objective-C iOS
 
 ## If you have the same qustion as mine: [iOS Objective-C RSA encrypt with only public key and descrypt with PHP](http://www.ideawu.com/blog/post/132.html)
 
-## History
-
-### 2015-09-26
-
-- New functions:
-  - `(NSString *)decryptString:(NSString *)str privateKey:(NSString *)privKey;`
-  - `(NSData *)decryptData:(NSData *)data privateKey:(NSString *)privKey;`
-
 ## Usage
 
 ```
@@ -20,6 +12,15 @@ NSString *pubkey = @"-----BEGIN PUBLIC KEY-----\nMIGfMA0GCSqGSIb3DQEBAQUAA4GNADC
 NSString *ret = [RSA encryptString:@"hello world!" publicKey:pubkey];
 NSLog(@"encrypted: %@", ret);
 ```
+
+## History
+
+### 2015-09-26
+
+- New functions:
+  - `(NSString *)decryptString:(NSString *)str privateKey:(NSString *)privKey;`
+  - `(NSData *)decryptData:(NSData *)data privateKey:(NSString *)privKey;`
+
 
 ## The PHP script for testing
 
--- encrypt.php
+++ encrypt.php
@@ -10,7 +10,15 @@ openssl_pkey_export($res, $private_key);
 $details = openssl_pkey_get_details($res);
 $public_key = $details["key"];
 
+echo "=====================\n";
+echo "create private key and public key:\n";
+echo "# PRIVATE:\n";
+echo str_replace("\n", "\\n", $private_key) . "\n";
+echo "# PUBLIC:\n";
+echo str_replace("\n", "\\n", $public_key) . "\n";
 #var_dump($private_key, $public_key);
+echo "=====================\n";
+echo "\n\n\n";
 
 $public_key = "-----BEGIN PUBLIC KEY-----\nMIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQDDI2bvVLVYrb4B0raZgFP60VXY\ncvRmk9q56QiTmEm9HXlSPq1zyhyPQHGti5FokYJMzNcKm0bwL1q6ioJuD4EFI56D\na+70XdRz1CjQPQE3yXrXXVvOsmq9LsdxTFWsVBTehdCmrapKZVVx6PKl7myh0cfX\nQmyveT/eqyZK1gYjvQIDAQAB\n-----END PUBLIC KEY-----";
 $private_key = "-----BEGIN PRIVATE KEY-----\nMIICdwIBADANBgkqhkiG9w0BAQEFAASCAmEwggJdAgEAAoGBAMMjZu9UtVitvgHS\ntpmAU/rRVdhy9GaT2rnpCJOYSb0deVI+rXPKHI9Aca2LkWiRgkzM1wqbRvAvWrqK\ngm4PgQUjnoNr7vRd1HPUKNA9ATfJetddW86yar0ux3FMVaxUFN6F0KatqkplVXHo\n8qXubKHRx9dCbK95P96rJkrWBiO9AgMBAAECgYBO1UKEdYg9pxMX0XSLVtiWf3Na\n2jX6Ksk2Sfp5BhDkIcAdhcy09nXLOZGzNqsrv30QYcCOPGTQK5FPwx0mMYVBRAdo\nOLYp7NzxW/File//169O3ZFpkZ7MF0I2oQcNGTpMCUpaY6xMmxqN22INgi8SHp3w\nVU+2bRMLDXEc/MOmAQJBAP+Sv6JdkrY+7WGuQN5O5PjsB15lOGcr4vcfz4vAQ/uy\nEGYZh6IO2Eu0lW6sw2x6uRg0c6hMiFEJcO89qlH/B10CQQDDdtGrzXWVG457vA27\nkpduDpM6BQWTX6wYV9zRlcYYMFHwAQkE0BTvIYde2il6DKGyzokgI6zQyhgtRJ1x\nL6fhAkB9NvvW4/uWeLw7CHHVuVersZBmqjb5LWJU62v3L2rfbT1lmIqAVr+YT9CK\n2fAhPPtkpYYo5d4/vd1sCY1iAQ4tAkEAm2yPrJzjMn2G/ry57rzRzKGqUChOFrGs\nlm7HF6CQtAs4HC+2jC0peDyg97th37rLmPLB9txnPl50ewpkZuwOAQJBAM/eJnFw\nF5QAcL4CYDbfBKocx82VX/pFXng50T7FODiWbbL4UnxICE0UBFInNNiWJxNEb6jL\n5xd0pcy9O2DOeso=\n-----END PRIVATE KEY-----";
