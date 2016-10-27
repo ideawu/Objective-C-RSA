ccb589c5fc2ac9bf969b00fffb9b0c3ce5cfae8f
chorsfield <chorsfield@defymedia.com>
2016-10-27 11:41:13 -0700

    pod update

--- Example/Pods/Target Support Files/Pods-RSA_Example/Pods-RSA_Example-umbrella.h
+++
@@ -1,6 +0,0 @@
-#import <UIKit/UIKit.h>
-
-
-FOUNDATION_EXPORT double Pods_RSA_ExampleVersionNumber;
-FOUNDATION_EXPORT const unsigned char Pods_RSA_ExampleVersionString[];
-
--- Example/Pods/Target Support Files/RSA/RSA-umbrella.h
+++ Example/Pods/Target Support Files/RSA/RSA-umbrella.h
@@ -1,5 +1,6 @@
 #import <UIKit/UIKit.h>
 
+#import "RSA.h"
 
 FOUNDATION_EXPORT double RSAVersionNumber;
 FOUNDATION_EXPORT const unsigned char RSAVersionString[];
